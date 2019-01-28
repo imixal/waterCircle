6871604f0090ca29c3b8620c56fedbee3af0d046
Ilya <imixxal@gmail.com>
2019-01-28 21:11:22 +0300

    init

---
+++ WaterCircle/Assets/Scripts/Movement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //variables
+    public float moveSpeed = 300;
+    public GameObject Character;
+
+    private Rigidbody2D CharacterBody;
+    private float ScreenWidth;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        ScreenWidth = Screen.width;
+        CharacterBody = Character.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int i = 0;
+        //loop over every touch found
+        while (i < Input.touchCount)
+        {
+            if (Input.GetTouch(i).position.x > ScreenWidth / 2)
+            {
+                //move right
+                RunCharacter(1.0f);
+            }
+            if (Input.GetTouch(i).position.x < ScreenWidth / 2)
+            {
+                //move left
+                RunCharacter(-1.0f);
+            }
+            ++i;
+        }
+    }
+    void FixedUpdate()
+    {
+#if UNITY_EDITOR
+		RunCharacter(Input.GetAxis("Horizontal"));
+#endif
+    }
+
+    private void RunCharacter(float horizontalInput)
+    {
+        //move player
+        CharacterBody.AddForce(new Vector2(horizontalInput * moveSpeed * Time.deltaTime, 0));
+    }
+}
---
+++ WaterCircle/Assets/Scripts/StartGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+
+
+    
+    void Start()
+    {
+
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "DropAnimation") { SceneManager.LoadScene("Start"); }
+
+
+    }
+
+}
