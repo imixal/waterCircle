4a029555fc8bef5f251073f15f00a40e1a573d12
Ilya <imixxal@gmail.com>
2019-01-29 23:29:40 +0300

    Added blob, blobRenderer

---
+++ WaterCircle/Assets/Scripts/Blob.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blob : MonoBehaviour
+{
+    public float Speed;
+
+    private float _acceleration = 0.001f;
+
+
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + Speed);
+        Speed += _acceleration;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+    }
+}
---
+++ WaterCircle/Assets/Scripts/BlobRenderer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobRenderer : MonoBehaviour
+{
+    [SerializeField] private float _period = 10f;
+    [SerializeField] private float _speedSeed = 0.7f;
+    [SerializeField] private float _minSpeed = 0.3f;
+    [SerializeField] private float _maxSpeed = 0.7f;
+    [SerializeField] private float _startRendererTime = 3f;
+
+    private float _timer;
+    private int _blobCount;
+
+    private Blob _blob;
+
+    private void Awake()
+    {
+        _blob = Resources.Load<Blob>("Blob");
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer > _startRendererTime && _timer / _period > _blobCount)
+        {
+            var randomValue = Random.value;
+            float x;
+            if ((int) (randomValue * 100) % 2 == 1)
+            {
+                x = Random.value * 50;
+            }
+            else
+            {
+                x = Random.value * 50 * -1;
+            }
+            var position = new Vector3(x, transform.position.y);
+            var blob = Instantiate(_blob, position, new Quaternion(180f, 0, 0, 0));
+            var speed = Random.value * _speedSeed;
+            if (speed > _maxSpeed)
+                speed = _maxSpeed;
+            else if (speed < _minSpeed)
+                speed = _minSpeed;
+            blob.Speed = speed;
+            _blobCount++;
+        }
+    }
+}
